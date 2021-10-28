80e39b0358491450914c93ea01918d47e66dad55
Armaan Gupta <armaan.gupta2020@gmail.com>
2021-10-28 11:59:45 +0530

    first commit

---
+++ backend js.js
@@ -0,0 +1,11 @@
+function TEDReadMore() {
+    window.open('https://www.ted.com/about/our-organization')
+}
+
+function TEDxReadMore() {
+    window.open('https://www.ted.com/about/programs-initiatives/tedx-program')
+}
+
+function TEDxSVVReadMore() {
+    window.open('message.html')
+}
